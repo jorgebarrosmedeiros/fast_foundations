e61fe92ff1d5813369029127a9fc29d630985f52
jorge-barros11 <jorge@getkoya.ai>
2023-06-01 12:48:35 -0300

    change test file

--- life_expectancy/tests/test_cleaning.py
+++ life_expectancy/tests/test_cleaning.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import argparse
 
-from life_expectancy.cleaning import clean_data
+from life_expectancy.cleaning import main
 from . import OUTPUT_DIR
 
 
@@ -15,8 +15,23 @@ def test_clean_data(pt_life_expectancy_expected):
         help="Region to filter the data (default is PT).",
     )
     args = parser.parse_args()
-    clean_data(region=args.region)
-    clean_data()
+    #clean_data(region=args.region)
+    #clean_data()
+    parser = argparse.ArgumentParser(description="Clean life expectancy data.")
+    parser.add_argument(
+        "--region",
+        default="PT",
+        help="Region to filter the data (default is PT).",
+    )
+    args = parser.parse_args()
+    main(
+        file_path_to_read="eu_life_expectancy_raw.tsv",
+        filename_to_save="pt_life_expectancy",
+        path_to_save="/Users/jorge/Documents/Repos/faast-foundations/production/" \
+                     "fast_foundations/life_expectancy/data",
+        region=args.region,
+    )
+    
     pt_life_expectancy_actual = pd.read_csv(OUTPUT_DIR / "pt_life_expectancy.csv")
     pd.testing.assert_frame_equal(
         pt_life_expectancy_actual, pt_life_expectancy_expected
